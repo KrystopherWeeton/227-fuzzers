ba3471281236a91e199f68e50ce47ec7441a57c8
Chester <chholtz@eng.ucsd.edu>
2021-05-09 16:32:56 -0700

    example binary

---
+++ hello_world.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   int hello = 1;
+   if (hello == 1){
+   	return 0;
+   } else {
+	return 1;
+   }
+}
